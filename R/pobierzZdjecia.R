7bc72e73c37128dc295e6993bc13dea2098d61d7
zozlak <zozlak@zozlak.org>
2016-08-19 12:05:16 +0200

    Dodana możliwość wyboru metody pobierania plików

--- R/pobierzZdjecia.R
+++ R/pobierzZdjecia.R
@@ -1,19 +1,26 @@
 #' Pobiera zdjęcia obejmujące wskazany punkt we wskazanym czasie
-#' @details
-#' Dokładniej pobierane są wszystkie zdjęcia w rozdzielczości 60 m / piksel.
+#' @details Dokładniej pobierane są wszystkie zdjęcia w rozdzielczości 60 m /
+#' piksel.
 #' 
-#' Zdjęcia zapisywane są pod nazwami "data_nazwaPasma.tiff" (skorygowane
-#' atmosferycznie) oraz "data_nazwaPasma_raw.tiff" (nieskorygowane
+#' Zdjęcia zapisywane są pod nazwami "data_nazwaPasma.tiff" (skorygowane 
+#' atmosferycznie) oraz "data_nazwaPasma_raw.tiff" (nieskorygowane 
 #' atmosferycznie).
-#' @param dlGeogr Długość geograficzna w stopniach. Długość E ma wartości dodatnie, długość W ujemne.
-#' @param szGeogr Szerokość geograficzna w stopniach. Szerokość N ma wartości dodatnie, szerokość S ujemne.
-#' @param dataOd data początku okresu, dla którego mają być pobrane zdjęcia w formacie RRRR-MM-DD
-#' @param dataDo data końca okresu, dla którego mają być pobrane zdjęcia w formacie RRRR-MM-DD
-#' @param katalog katalog, do którego zapisane zostaną zdjęcia (domyślnie obecny katalog roboczy)
+#' @param dlGeogr Długość geograficzna w stopniach. Długość E ma wartości
+#'   dodatnie, długość W ujemne.
+#' @param szGeogr Szerokość geograficzna w stopniach. Szerokość N ma wartości
+#'   dodatnie, szerokość S ujemne.
+#' @param dataOd data początku okresu, dla którego mają być pobrane zdjęcia w
+#'   formacie RRRR-MM-DD
+#' @param dataDo data końca okresu, dla którego mają być pobrane zdjęcia w
+#'   formacie RRRR-MM-DD
+#' @param katalog katalog, do którego zapisane zostaną zdjęcia (domyślnie obecny
+#'   katalog roboczy)
+#' @param method metoda pobierania plików (patrz parametr \code{method} funkcji 
+#'   \code{\link[utils]{download.file}})
 #' @return ramka danych opisująca pobrane zdjęcia
 #' @import dplyr
 #' @export
-pobierzZdjecia = function(dlGeogr, szGeogr, dataOd, dataDo, progChmur = 50, katalog = '.'){
+pobierzZdjecia = function(dlGeogr, szGeogr, dataOd, dataDo, progChmur = 50, katalog = '.', method = 'auto'){
   req = httr::GET(
     'http://s2.boku.eodc.eu/image',
     query = list(
@@ -39,7 +46,7 @@ pobierzZdjecia = function(dlGeogr, szGeogr, dataOd, dataDo, progChmur = 50, kata
   pb = txtProgressBar(0, nrow(images), style = 3)
   for (i in seq_along(images$imageId)) {
     setTxtProgressBar(pb, i)
-    download.file(images$url[i], images$file[i], mode = 'wb', quiet = TRUE)
+    download.file(images$url[i], images$file[i], mode = 'wb', quiet = TRUE, method = method)
   }
   cat("\n")
   
